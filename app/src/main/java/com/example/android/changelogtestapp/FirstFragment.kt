e50e6751e12db7941bdee5def969259b23b3b6ab
Gabor <gaborpeter11@gmail.com>
2022-01-13 15:58:47 +0100

    commit

--- app/src/main/java/com/example/android/changelogtestapp/FirstFragment.kt
+++ app/src/main/java/com/example/android/changelogtestapp/FirstFragment.kt
@@ -13,19 +13,21 @@ import com.example.android.changelogtestapp.databinding.FragmentFirstBinding
  */
 class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
-
+private var _binding: FragmentFirstBinding? = null
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    //print
+    //changes
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
+      _binding = FragmentFirstBinding.inflate(inflater, container, false)
+      return binding.root
 
     }
 
@@ -37,7 +39,7 @@ class FirstFragment : Fragment() {
         }
     }
 
-    override fun onDestroyView() {
+override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
